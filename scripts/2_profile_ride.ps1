65bb3d45c1d24238750284c9d6a460e00b88f490
Benjamin Bara <bbara93@gmail.com>
2019-10-09 21:58:06 +0200

    scripts: improve timestamp

--- scripts/2_profile_ride.ps1
+++ scripts/2_profile_ride.ps1
@@ -1,3 +1,3 @@
 .\environment\Scripts\activate.ps1
 New-Item -Path ".\profiles" -ItemType "directory" -Force | Out-Null
-python -m cProfile -o ".\profiles\$(Get-Date -Format "MM-dd-yyyy_HH-mm").prof" -m ride .\tests\generated\
+python -m cProfile -o ".\profiles\$(Get-Date -Format "yyyy-MM-dd_HH-mm-ss").prof" -m ride .\tests\generated\
